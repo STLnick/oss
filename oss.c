40e890d289f44dcc8e4ce53206442b7f8f709e90
nicholas ray <nicholas@hoare7.cs.umsl.edu>
2020-10-17 08:03:00 -0500

    :tada: Initial commit for OSS project

---
+++ oss.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main (int argc, char *argv)
+{
+
+  return 0;
+}
